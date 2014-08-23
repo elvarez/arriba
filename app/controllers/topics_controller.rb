bfb609938aa0cf93b232a955e15cad004c741aff
szuletett <szuletett@gmail.com>
2014-08-23 17:24:31 +0400

    associated topics and posts. Refactored

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,47 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+    authorize @topics
+
+  end
+
+  def new
+    @topic = Topic.new
+    authorize @topic
+
+  end
+
+  def create
+    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    authorize @topic
+    if @topic.save
+      redirect_to @topic, notice: "Topic was save successfully"
+    else
+      flash[:error] = "Error creating topic. Try again."
+      render :new
+    end
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+    @posts = @topic.posts
+    authorize @topic
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+    authorize @topic
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    authorize @topic
+    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+      redirect_to @topic
+    else
+      flash[:error] = "Error saving topic. Try again."
+      render :edit
+    end
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,23 @@ end
 
 users = User.all
 
+
+# create topics
+15.times do
+  Topic.create(
+               name: Faker::Lorem.sentence,
+               description: Faker::Lorem.paragraph
+               )
+end
+topics = Topic.all
+
+
+
 # create posts
 50.times do
   Post.create(
               user: users.sample,
+              topic: topics.sample,
               title: Faker::Lorem.sentence,
               body: Faker::Lorem.paragraph
               )
@@ -30,13 +43,32 @@ posts = Post.all
                  )
   end
 
-User.first.update_attributes!(
-                              email: 'elvarez@gmail.com',
-                              password: 'helloworld',
-                              )
+# create admin
+admin = User.new(
+                 name: 'Admin User',
+                 email: 'admin@example.com',
+                 password: 'helloworld',
+                 role: 'admin'
+                 )
+admin.skip_confirmation!
+admin.save!
 
-puts "Seed finished"
-puts "#{User.count} users created!"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+# create moderator
+moderator = User.new(
+                 name: 'Moderator User',
+                 email: 'moderator@example.com',
+                 password: 'helloworld',
+                 role: 'moderator'
+                 )
+moderator.skip_confirmation!
+moderator.save!
+
+# create member
+member = User.new(
+                 name: 'Member User',
+                 email: 'member@example.com',
+                 password: 'helloworld',
+                 )
+member.skip_confirmation!
+member.save!
 
