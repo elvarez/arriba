125a1225bc12b27388d0aeb94d6ecc24cdcbbada
szuletett <szuletett@gmail.com>
2014-08-24 17:26:32 +0400

    markdown

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'devise'
 
 gem 'pundit'
 
+gem 'redcarpet'
+
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,11 @@ def form_group_tag(errors, &block)
   end
 end
 
+def markdown(text)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render text).html_safe
+end
+
 end
