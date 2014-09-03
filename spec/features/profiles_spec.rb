90f31f6f5db3b2a94a2c1a693935f7831267f47b
szuletett <szuletett@gmail.com>
2014-09-03 17:38:42 +0200

    test for admin user

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 describe 'Visiting profiles' do
 
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before do
-    @user = authenticated_user
+    @user = authenticated_user(role: 'admin')
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "A comment")
+    @comment = Comment.new(user: @user, post: @post, body: "A comment")
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save
   end
@@ -15,6 +17,20 @@ describe 'Visiting profiles' do
   describe "not signed in" do
 
     it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+
+  end
+
+  describe "signed in" do
+      
+    it "shows profile" do
+      login_as(@user, :scope => :user)
       visit user_path(@user)
       expect(current_path).to eq(user_path(@user))
 
